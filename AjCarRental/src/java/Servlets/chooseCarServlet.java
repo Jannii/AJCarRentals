11da9404fd41c0e7a2649f47dd477d08138386ba
Axel Malmberg <axel_malmberg@hotmail.com>
2017-01-18 18:24:50 +0100

    asd

--- AjCarRental/src/java/Servlets/chooseCarServlet.java
+++ AjCarRental/src/java/Servlets/chooseCarServlet.java
@@ -10,6 +10,12 @@ import EntityBeans.Car;
 import Hibernate.HybernateUtil;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -70,35 +76,44 @@ public class chooseCarServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         for (int i = 0; i < 1000; i++) {
-        if (request.getParameter("" + i) != null) {
-            System.out.println("the car id you choose: " + i);
-            
-            HybernateUtil hu = new HybernateUtil();
-            SessionFactory sessionFactory = hu.getSessionFactory();
+            if (request.getParameter("" + i) != null) {
+                System.out.println("the car id you choose: " + i);
 
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-            Car c = new Car();
-            c = (Car) session.get(Car.class, i);
-            session.getTransaction().commit();
-            
-            int price = c.getDailyPrice();
-            
-            statefulBean sfb = new statefulBean();
-            String pickUpDate = sfb.getPickUpDate();
-            String dropOfDate = sfb.getDropOfDate();
-            
-            
-            request.setAttribute("carName", c.getCarName());
-            request.setAttribute("carType", c.getCarType());
-            request.setAttribute("carHome", c.getCarHome());
-            request.setAttribute("carLocation", c.getCarlocation());
-            request.setAttribute("carPrice", price);
-            request.getRequestDispatcher("paypal.jsp").forward(request, response);
-            break;
+                HybernateUtil hu = new HybernateUtil();
+                SessionFactory sessionFactory = hu.getSessionFactory();
+
+                Session session = sessionFactory.openSession();
+                session.beginTransaction();
+                Car c = new Car();
+                c = (Car) session.get(Car.class, i);
+                session.getTransaction().commit();
+
+                int price = c.getDailyPrice();
+
+                statefulBean sfb = new statefulBean();
+                String pickUpDate = sfb.getPickUpDate();
+                String dropOfDate = sfb.getDropOfDate();
+                Date startDate;
+                Date endDate;
+                DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
+                try {
+                    startDate = df.parse(pickUpDate);
+                    endDate = df.parse(dropOfDate);
+                } catch (ParseException ex) {
+                    Logger.getLogger(chooseCarServlet.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+
+                request.setAttribute("carName", c.getCarName());
+                request.setAttribute("carType", c.getCarType());
+                request.setAttribute("carHome", c.getCarHome());
+                request.setAttribute("carLocation", c.getCarlocation());
+                request.setAttribute("carPrice", price);
+                request.getRequestDispatcher("paypal.jsp").forward(request, response);
+                break;
+            }
         }
-        }
-        
+
         processRequest(request, response);
     }
 
