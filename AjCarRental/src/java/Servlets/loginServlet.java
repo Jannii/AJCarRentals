afafdce366f9ff215d1fe59f524c15bc23ee037b
Jannii <Johancn83@gmail.com>
2017-01-18 21:13:56 +0100

    log in member

--- AjCarRental/src/java/Servlets/loginServlet.java
+++ AjCarRental/src/java/Servlets/loginServlet.java
@@ -23,6 +23,7 @@ import static java.util.Collections.list;
 import java.util.List;
 import org.hibernate.Query;
 import Beans.Userinfo;
+import java.util.LinkedList;
 
 /**
  *
@@ -75,85 +76,75 @@ public class loginServlet extends HttpServlet {
         if (request.getParameter("Submit") != null) {
             String user = request.getParameter("username");
             String pass = request.getParameter("password");
-            Userinfo u = new Userinfo();
+            Userinfo ui = new Userinfo();
             HybernateUtil hu = new HybernateUtil();
             SessionFactory sessionFactory = hu.getSessionFactory();
+            
             Session session = sessionFactory.getCurrentSession();
-            Transaction tx = session.beginTransaction();
+            session.beginTransaction();
 
             String queryString = "Select l from Login l where l.username= :user";
             Query query = session.createQuery(queryString);
             query.setParameter("user", user);
             List<EntityBeans.Login> loginlist = query.list();
-            System.out.println("cretaing list");
-            for (EntityBeans.Login login : loginlist) {
-                System.out.println("in for loop");
-                
-                if (login.getUsername().equals(user) && login.getUserpass().equals(pass)) {
-                    System.out.println("Success");
-                    try {
-                        int  id = Integer.parseInt(loginlist.get(0).toString());
-                        SessionFactory sessionFactory1 = hu.getSessionFactory();
-                        Session session1 = sessionFactory1.getCurrentSession();
-                        Transaction tx1 = session1.beginTransaction();
-                        String queryString1 = "Select * from user where Login_idLogin= :id";
-                        Query query1 = session1.createSQLQuery(queryString);
-                        query.setParameter("id", id);
-                        List<EntityBeans.User> User = query.list();
-                        
-                        u.setName(User.get(1).toString());
-                        u.setAdress(User.get(2).toString());
-                        u.setEmail(User.get(3).toString());
-                        u.setPhone(User.get(4).toString());
-                        if (login.getStatus().equals("member")) {
-                            System.out.println("member");
-                            request.getRequestDispatcher("homemember.jsp").forward(request, response);
-                        } else if (login.getStatus().equals("admin")) {
-                            request.getRequestDispatcher("homeadmin.jsp").forward(request, response);
-                        }
+            session.getTransaction().commit();
 
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
+            int id = loginlist.get(0).getIdLogin();
+            
+            
+
+               session = sessionFactory.openSession();
+               session.beginTransaction();
+               User u = new User();        
+               u = (User) session.get(User.class, id);
+               session.getTransaction().commit();
+               System.out.println(u.getName());
+
+
+        for (EntityBeans.Login login : loginlist) {
+            System.out.println("in for loop");
+
+            if (login.getUsername().equals(user) && login.getUserpass().equals(pass)) {
+                System.out.println("Success");
+                try {
+
+                    if (login.getStatus().equals("member")) {
+                        System.out.println("member");
+                        request.getRequestDispatcher("homemember.jsp").forward(request, response);
+                    } else if (login.getStatus().equals("admin")) {
+                        request.getRequestDispatcher("homeadmin.jsp").forward(request, response);
                     }
 
-                } else {
-                    String error = "Your Username and/or Password dont exist";
-                    System.out.println("redirect to login in for");
-                    request.setAttribute("error", error);
-                    request.getRequestDispatcher("login.jsp").forward(request, response);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
                 }
-            }
-            if (loginlist.size() == 0) {
+
+            } else {
                 String error = "Your Username and/or Password dont exist";
-                System.out.println("redirekt to login utside for");
+                System.out.println("redirect to login in for");
                 request.setAttribute("error", error);
-                request.getRequestDispatcher("login.jsp").forward(request, response);
+                request.getRequestDispatcher("payment.jsp").forward(request, response);
             }
-            System.out.println("tx.commit");
-            tx.commit();
-            hu.close();
-//            try {
-//               Session session = hu.getSessionFactory().openSession();
-//               session.beginTransaction();
-//               u = (Login) session.get(Login.class, 2);
-//               String name  = u.getUsername();
-//               System.out.println(name);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-
         }
-
-        processRequest(request, response);
+        if (loginlist.size() == 0) {
+            String error = "Your Username and/or Password dont exist";
+            System.out.println("redirekt to login utside for");
+            request.setAttribute("error", error);
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
+//        hu.close();
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
+    processRequest(request, response);
+}
+
+/**
+ * Returns a short description of the servlet.
+ *
+ * @return a String containing servlet description
+ */
+@Override
+        public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
