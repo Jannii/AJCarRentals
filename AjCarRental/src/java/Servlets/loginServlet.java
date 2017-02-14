19b9ea8ec5ffbfb0228e7191935a78c27087e4fd
Axel Malmberg <axel_malmberg@hotmail.com>
2017-02-14 12:53:53 +0100

    update something went wrong

--- AjCarRental/src/java/Servlets/loginServlet.java
+++ AjCarRental/src/java/Servlets/loginServlet.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package Servlets;
-
+ 
 import EntityBeans.Login;
 import EntityBeans.User;
 import java.io.IOException;
@@ -22,17 +22,16 @@ import Hibernate.HybernateUtil;
 import static java.util.Collections.list;
 import java.util.List;
 import org.hibernate.Query;
-import Beans.Userinfo;
 import java.util.LinkedList;
 import Beans.statefulBean;
-
+ 
 /**
  *
  * @author Johan Nilsson
  */
 @WebServlet(name = "loginServlet", urlPatterns = {"/loginServlet"})
 public class loginServlet extends HttpServlet {
-
+ 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -45,9 +44,9 @@ public class loginServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-
+ 
     }
-
+ 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -62,7 +61,7 @@ public class loginServlet extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
+ 
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -75,33 +74,46 @@ public class loginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         if (request.getParameter("Submit") != null) {
+            System.out.println("Submit pressed");
             String user = request.getParameter("username");
             String pass = request.getParameter("password");
-            Userinfo ui = new Userinfo();
             HybernateUtil hu = new HybernateUtil();
             SessionFactory sessionFactory = hu.getSessionFactory();
-
+ 
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
-
+            System.out.println("Session beginTransaction");
+ 
             String queryString = "Select * from Login where Login.username= :user";
             Query query = session.createSQLQuery(queryString);
+            System.out.println("querylist made");
             query.setParameter("user", user);
-            List<EntityBeans.Login> loginlist = query.list();
+            List loginlist = query.list();
+            Object[] obj = null;
+            for(Object o : loginlist){
+                obj =(Object[])o;
+                System.out.println(obj[0]);
+            }
+            System.out.println("added query.list to List ");
             session.getTransaction().commit();
-            
-            int id = loginlist.get(0).getIdLogin();
-
+            System.out.println("commit");
+            int id = Integer.parseInt(obj[0].toString());
+            String gotname =obj[1].toString();
+            String gotpass =obj[2].toString();
+            String gotstatus=obj[3].toString();
+ 
             session = sessionFactory.openSession();
             session.beginTransaction();
             User u = new User();
+            System.out.println("new user");
             u = (User) session.get(User.class, id);
             session.getTransaction().commit();
-
-            for (EntityBeans.Login login : loginlist) {
+            System.out.println("commit");
+ 
+            for (int i = 0; i < loginlist.size(); i++) {
                 System.out.println("in for loop");
-
-                if (login.getUsername().equals(user) && login.getUserpass().equals(pass)) {
+ 
+                if (gotname.equals(user) && gotpass.equals(pass)) {
                     System.out.println("Success");
                     statefulBean stb = new statefulBean();
                     stb.setName(u.getName());
@@ -109,8 +121,8 @@ public class loginServlet extends HttpServlet {
                     stb.setMail(u.getEmail());
                     stb.setPhone(u.getPhone());
                     try {
-
-                        if (login.getStatus().equals("member")) {
+ 
+                        if (gotstatus.equals("member")) {
                             session = sessionFactory.getCurrentSession();
                             session.beginTransaction();
                             queryString = "SELECT * FROM Office";
@@ -118,8 +130,8 @@ public class loginServlet extends HttpServlet {
                             System.out.println("Query::::" + query.getQueryString().toString());;
                             request.setAttribute("Loc", query.list());
                             System.out.println("member");
-                            request.getRequestDispatcher("homemember.jsp").forward(request, response);
-                        } else if (login.getStatus().equals("admin")) {
+                            request.getRequestDispatcher("homecasual.jsp").forward(request, response);
+                        } else if (gotstatus.equals("admin")) {
                             session = sessionFactory.getCurrentSession();
                             session.beginTransaction();
                             queryString = "SELECT * FROM Office";
@@ -127,13 +139,13 @@ public class loginServlet extends HttpServlet {
                             System.out.println("Query::::" + query.getQueryString().toString());;
                             request.setAttribute("Loc", query.list());
                             System.out.println("member");
-                            request.getRequestDispatcher("homeadmin.jsp").forward(request, response);
+                            request.getRequestDispatcher("homecasual.jsp").forward(request, response);
                         }
-
+ 
                     } catch (Exception ex) {
                         ex.printStackTrace();
                     }
-
+ 
                 } else {
                     String error = "Your Username and/or Password dont exist";
                     System.out.println("redirect to login in for");
@@ -141,7 +153,7 @@ public class loginServlet extends HttpServlet {
                     request.getRequestDispatcher("payment.jsp").forward(request, response);
                 }
             }
-            if (loginlist.size() == 0) {
+            if (obj.length == 0) {
                 String error = "Your Username and/or Password dont exist";
                 System.out.println("redirekt to login utside for");
                 request.setAttribute("error", error);
@@ -149,10 +161,10 @@ public class loginServlet extends HttpServlet {
             }
 //        hu.close();
         }
-
+ 
         processRequest(request, response);
     }
-
+ 
     /**
      * Returns a short description of the servlet.
      *
@@ -162,5 +174,5 @@ public class loginServlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
+ 
 }
